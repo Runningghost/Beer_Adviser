bc963b811ccad4a689488c5a02af25cda9f73558
runningghost <runningghost16@gmail.com>
2016-05-24 22:56:18 -0400

    added class DisplayBeer

--- app/src/main/java/com/ghost/dmac/beeradviser/DisplayBeer.java
+++ app/src/main/java/com/ghost/dmac/beeradviser/DisplayBeer.java
@@ -1,6 +1,5 @@
 package com.ghost.dmac.beeradviser;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import java.io.IOException;
 
 
 public class FindBeerActivity extends AppCompatActivity {
