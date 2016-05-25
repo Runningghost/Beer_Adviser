3861fc230982b22bf5da191e11c15d9edc0fe4af
runningghost <runningghost16@gmail.com>
2016-05-24 22:47:59 -0400

    added class DisplayBeer

---
+++ app/src/main/java/com/ghost/dmac/beeradviser/DisplayBeer.java
@@ -0,0 +1,101 @@
+package com.ghost.dmac.beeradviser;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Dmac on 5/24/2016.
+ */
+public class DisplayBeer extends AppCompatActivity {
+
+    String url = "https://www.beerknurd.com/user";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_beer_info);
+        GetBeerInfo website = new GetBeerInfo();
+        website.execute(url);
+    }
+
+    public class GetBeerInfo extends AsyncTask<String, String, String> {
+        TextView beertaste = (TextView) findViewById(R.id.beer_tasted);
+
+
+
+        // Given a URL, establishes an HttpUrlConnection and retrieves
+// the web page content as a InputStream, which it returns as
+// a string.
+        @Override
+        protected String doInBackground(String... params) {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            String contentAsString = null;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                //int response = conn.getResponseCode();
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                contentAsString = readIt(is, len);
+                //return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return contentAsString;
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            // execution of result of Long time consuming operation
+            beertaste.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // Things to be done before execution of long running operation. For
+            // example showing ProgessDialog
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            beertaste.setText(text[0]);
+            // Things to be done while execution of long running operation is in
+            // progress. For example updating ProgessDialog
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
@@ -1,22 +1,13 @@
 package com.ghost.dmac.beeradviser;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 
 public class FindBeerActivity extends AppCompatActivity {
-    String url = "https://www.beerknurd.com/user";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,78 +15,12 @@ public class FindBeerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_beer);
     }
 
-    public void onClickFindBeer(View view) throws IOException {
-        getUrl website = new getUrl();
-        website.execute(url);
+    public void onClickFindBeer(View view) {
+        Intent intent = new Intent(this, DisplayBeer.class);
+        startActivity(intent);
+
     }
 
-
-    public class getUrl extends AsyncTask<String, String, String> {
-        TextView cheese = (TextView) findViewById(R.id.foobar);
-
-        // Given a URL, establishes an HttpUrlConnection and retrieves
-// the web page content as a InputStream, which it returns as
-// a string.
-        @Override
-        protected String doInBackground(String... params) {
-            InputStream is = null;
-            // Only display the first 500 characters of the retrieved
-            // web page content.
-            int len = 500;
-
-            String contentAsString = null;
-            try {
-                URL url = new URL(params[0]);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                // Starts the query
-                conn.connect();
-                //int response = conn.getResponseCode();
-                is = conn.getInputStream();
-
-                // Convert the InputStream into a string
-                contentAsString = readIt(is, len);
-                //return contentAsString;
-
-                // Makes sure that the InputStream is closed after the app is
-                // finished using it.
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return contentAsString;
-        }
-
-        public String readIt(InputStream stream, int len) throws IOException {
-            Reader reader = null;
-            reader = new InputStreamReader(stream, "UTF-8");
-            char[] buffer = new char[len];
-            reader.read(buffer);
-            return new String(buffer);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            // execution of result of Long time consuming operation
-            cheese.setText(result);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            // Things to be done before execution of long running operation. For
-            // example showing ProgessDialog
-        }
-
-        @Override
-        protected void onProgressUpdate(String... text) {
-            cheese.setText(text[0]);
-            // Things to be done while execution of long running operation is in
-            // progress. For example updating ProgessDialog
-        }
-    }
 
 }
 
