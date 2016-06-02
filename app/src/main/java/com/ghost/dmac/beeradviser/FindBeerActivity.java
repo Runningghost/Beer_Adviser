245e63bf3b1ee8b5d282ce0c248c5188e01a36de
runningghost <runningghost16@gmail.com>
2016-06-02 00:34:38 -0400

    Add username and password edittext

--- app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/ghost/dmac/beeradviser/FindBeerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class FindBeerActivity extends AppCompatActivity {
@@ -15,7 +16,14 @@ public class FindBeerActivity extends AppCompatActivity {
     }
 
     public void onClickFindBeer(View view) {
+        EditText userView = (EditText) findViewById(R.id.user);
+        String userText = userView.getText().toString();
+        EditText passView = (EditText) findViewById(R.id.pass);
+        String passText = userView.getText().toString();
         Intent intent = new Intent(this, DisplayBeer.class);
+        intent.putExtra("username", userText);
+        intent.putExtra("password", passText);
+
         startActivity(intent);
     }
 
